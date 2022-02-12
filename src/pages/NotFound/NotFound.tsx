ed5d6abd72a97a3a09939421c2a69f1d15133bb5
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2022-02-12 12:12:08 -0300

    feat: context/hooks

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Assets } from '../../assets';
+import { Atoms } from '../../shared/components';
+
+import * as S from './styles';
+
+const NotFound: React.FC = () => {
+  return (
+    <S.Container>
+      <Atoms.Text weight="bold" size="small">
+        Sorry, the page you are looking for does not exist.
+      </Atoms.Text>
+      <Assets.NotFound />
+    </S.Container>
+  );
+};
+
+export default NotFound;
---
+++ src/shared/hooks/useJobs.ts
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { JobsContext } from '../contexts';
+
+export function useJobs() {
+  const context = useContext(JobsContext);
+
+  if (!context) {
+    throw new Error('useJobs must be used within an JobsProvider');
+  }
+
+  return context;
+}
