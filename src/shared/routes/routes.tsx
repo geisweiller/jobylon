3e68996af35ff9f07b3bf54b18d6007440b20c1f
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2022-02-12 12:10:59 -0300

    feat: service/routes

---
+++ src/shared/routes/routes.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { JobsPage, PageNotFound } from '../../pages';
+import { DefaultTemplate } from '../templates';
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <DefaultTemplate>
+        <Routes>
+          <Route path="/" element={<Navigate to="/jobs" />} />
+          <Route path="/jobs" element={<JobsPage />} />
+          <Route path="/my-jobs" element={<PageNotFound />} />
+          <Route path="/reports" element={<PageNotFound />} />
+          <Route path="/chat" element={<PageNotFound />} />
+        </Routes>
+      </DefaultTemplate>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
